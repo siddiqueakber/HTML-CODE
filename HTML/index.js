cf3595f9d3951f2a47567a4fefb33681b341ed36
Waleed Ahmed <waleedahmedbarozai@gmail.com>
2024-05-12 22:48:05 +0500

    Waleed Wala Code

---
+++ HTML/index.js
@@ -0,0 +1,37 @@
+// Variables  (Dynamic vs Static Languages)
+// FOr, Function, While, If , Ternary, Arrays, Arrow Function, Spread Operator, Rest Operator, Object Destructuring, Array Functions,
+
+// function sum(age) {
+//   age = age + 1;
+// }
+
+// console.log(sum(19));
+
+// let Summed = (ab) => (ab = ab + 1);
+
+// console.log(Summed(6));
+
+// let list1 = [1, 2, 3];
+// let list2 = [4, 5, 6];
+
+// let list = [36, ...list1, ...list2];
+
+// console.log(list);
+
+//DRY = "DON'T REPEAT YOURSELF"
+
+let ages = [12, 0, 11, 41, 6, 8, 66];
+
+let filteredData = ages.filter((age) => age >= 18);
+
+console.log(filteredData);
+
+// list1.map((num) => {
+//   console.log(
+//     "I am a/an",
+//     num % 2 === 0 ? " Even" : "Odd",
+//     " Number ",
+//     num,
+//     "\n"
+//   );
+// });
